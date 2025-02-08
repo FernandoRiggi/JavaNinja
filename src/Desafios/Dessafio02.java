418b361f2f036a1e673c9d525eeb4f2f8e9111fe
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2025-02-07 22:59:21 -0300

    atualizacao desafio 2

--- src/Desafios/Dessafio02.java
+++ src/Desafios/Dessafio02.java
@@ -7,9 +7,10 @@ public class Dessafio02 {
         Scanner entrada = new Scanner(System.in);
         int opc = 0;
         int numeroMaximoNinja;
+        int ninjasCadastrados =0;
         System.out.println("Entre com a quantidade de ninjas que deseja adicionar: ");
         numeroMaximoNinja = entrada.nextInt();
-        String[] ninja = new String[numeroMaximoNinja + 10]; //caso o user queira adicionar mais e não sabia na hora
+        String[] ninja = new String[numeroMaximoNinja];
         while (opc != 3) {
             System.out.println("\n===== Menu Ninja =====");
             System.out.println("1. Cadastrar Ninja");
@@ -21,9 +22,14 @@ public class Dessafio02 {
             switch (opc) {
                 case 1:
                     System.out.println("Entrando no menu Cadastrar Ninja");
-                    for (int i = 0; i < numeroMaximoNinja; i++) {
-                        System.out.println("Entre com o nome do ninja que deseja adicionar: ");
-                        ninja[i] = entrada.nextLine();
+                    if (ninjasCadastrados < numeroMaximoNinja) {
+                        for (int i = 0; i < numeroMaximoNinja; i++) {
+                            System.out.println("Entre com o nome do ninja que deseja adicionar: ");
+                            ninja[i] = entrada.nextLine();
+                            ninjasCadastrados++;
+                        }
+                    }else{
+                        System.out.println("A lista de ninja já está cheia");
                     }
                     break;
                 case 2:
@@ -32,15 +38,17 @@ public class Dessafio02 {
                         for(int i=0; i <numeroMaximoNinja; i++){
                             System.out.println("Nome do ninja "+i+":  " + ninja[i]);
                         }
-                        break;
                     }
+                    else{
+                        System.out.println("Não há nninjas cadastrados");
+                    }
+                    break;
                 case 3:
                     System.out.println("Encerrando programa");
-                    System.exit(0);
+                    break;
                 default:
                     System.out.println("Não há ninjas cadastrados!");
                     break;
-
                 }
             }
         }
